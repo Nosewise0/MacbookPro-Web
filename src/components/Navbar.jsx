4bf57c0060d45b257f15dc6c0eabe93459075073
Nosewise0 <nonsaker021@gmail.com>
2026-06-10 09:41:59 +0800

    Macbook pro

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { navLinks } from "../constants";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src="../public/logo (4).svg" alt="Apple Logo" />
+
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.name}>
+              <a href={item.href}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div clssName="flex-center gap-3">
+          <button>
+            <img src="../public/search (1).svg" alt="Search" />
+          </button>
+          <button>
+            <img src="../public/Cart (4).svg" alt="Cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+const navLinks = [
+     { name: "Home", href: "#" },
+     { name: "About", href: "#" },
+    { name: "Store", href: "#" },
+    { name: "Contact", href: "#" }
+];
+
+export { navLinks };
